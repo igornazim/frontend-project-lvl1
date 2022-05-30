6cfb92fbc2744aa8b4b8db0be08161ab047afacd
Igor Nazim <103509073+igornazim@users.noreply.github.com>
2022-05-30 23:56:26 +0300

    Update even-number.js

--- src/even-number.js
+++ src/even-number.js
@@ -14,14 +14,14 @@ const isEvenGame = () => {
     const value = random.integer(1, 100);
     const expectedAnswer = isEven(value) ? 'yes' : 'no';
     const userAnswer = readlineSync.question(value);
-    if (userAnswer === expectedAnswer) {
-      console.log(`Your answer: ${userAnswer}`);
-      console.log('Correct!');
-      i += 1;
+    if (userAnswer !== expectedAnswer) {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${expectedAnswer}.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
     }
-    console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${expectedAnswer}.`);
-    console.log(`Let's try again, ${userName}!`);
-    return;
+    console.log(`Your answer: ${userAnswer}`);
+    console.log('Correct!');
+    i += 1;
   };
   let i = 0;
   while (i < rounds) {
