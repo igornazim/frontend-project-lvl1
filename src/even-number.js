69f267a2ef2815a502dd9a2ff1be3ccd8608f6b6
Igor Nazim <103509073+igornazim@users.noreply.github.com>
2022-05-30 14:20:22 +0300

    Update even-number.js

--- src/even-number.js
+++ src/even-number.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
 import { Random } from "random-js";
+
 const random = new Random();
 const value = random.integer(1, 100);
 const isEven = number => number % 2 === 0;
@@ -12,9 +13,8 @@ const isEvenGame = () => {
   const expectedAnswer = isEven(value) ? 'yes' : 'no';
   const userUnswer = readlineSync.question(value);
   if (userUnswer === expectedAnswer) {
-    console.log('Correct!')
-  }
-  else console.log("Wrong answer!")
+    console.log('Correct!');
+  } else console.log('Wrong answer!')
 };
 
 export default isEvenGame;
