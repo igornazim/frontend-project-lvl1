4bd3aa17ebffeea442b946e5d150f20b32cbb5a6
Igor Nazim <103509073+igornazim@users.noreply.github.com>
2022-05-30 23:38:26 +0300

    Update even-number.js

--- src/even-number.js
+++ src/even-number.js
@@ -18,8 +18,8 @@ const isEvenGame = () => {
       console.log(`Your answer: ${userAnswer}`);
       console.log('Correct!');
       i += 1;
-    } else console.log('Wrong answer!');
-    console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${expectedAnswer}. Let's try again, ${userName}!`);
+    } else console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${expectedAnswer}. Let's try again, ${userName}!`);
+    return;
   };
   let i = 0;
   while (i < rounds) {
