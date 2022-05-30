16c4b23bacc6f3e169420d334fe018fa1786ec0d
Igor Nazim <igor.mainevent@gmail.com>
2022-05-30 12:51:14 +0300

    five step

---
+++ src/even-number.js
@@ -0,0 +1,20 @@
+import readlineSync from 'readline-sync';
+import { Random } from "random-js";
+const random = new Random();
+const value = random.integer(1, 100);
+const inctruction = 'Answer "yes" if the number is even, otherwise answer 
+"no".';
+const isEven = (value % 2 === 0) ? 'yes' : 'no';
+
+const isEvenGame = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(inctruction);
+  const userUnswer = readlineSync.question(value);
+  if (userUnswer === isEven(value)) {
+    console.log('Correct!')
+    else console.log(''yes' is wrong answer ;(. Correct answer was 'no'.
+Let's try again, Bill!')
+  }
+};
