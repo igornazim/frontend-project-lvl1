77774869d8546e5e8a948dfbfc5c57ea6eac3c19
Igor Nazim <103509073+igornazim@users.noreply.github.com>
2022-05-30 13:24:41 +0300

    Update even-number.js

--- src/even-number.js
+++ src/even-number.js
@@ -2,15 +2,14 @@ import readlineSync from 'readline-sync';
 import { Random } from "random-js";
 const random = new Random();
 const value = random.integer(1, 100);
-const inctruction = 'Answer "yes" if the number is even, otherwise answer 
-"no".';
 const isEven = (value % 2 === 0) ? 'yes' : 'no';
 
 const isEvenGame = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  console.log(inctruction);
+  console.log('Answer "yes" if the number is even, otherwise answer 
+"no".');
   const userUnswer = readlineSync.question(value);
   if (userUnswer === isEven(value)) {
     console.log('Correct!')
