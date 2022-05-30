65f5dc4bdccee3917539e0ba0070fbfa1fd9c1cb
Igor Nazim <103509073+igornazim@users.noreply.github.com>
2022-05-30 14:25:37 +0300

    Update even-number.js

--- src/even-number.js
+++ src/even-number.js
@@ -4,17 +4,23 @@ import { Random } from "random-js";
 const random = new Random();
 const value = random.integer(1, 100);
 const isEven = number => number % 2 === 0;
+const rounds = 3;
 
 const isEvenGame = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  const expectedAnswer = isEven(value) ? 'yes' : 'no';
-  const userUnswer = readlineSync.question(value);
-  if (userUnswer === expectedAnswer) {
-    console.log('Correct!');
-  } else console.log('Wrong answer!')
+  while (i <= rounds) {
+    const expectedAnswer = isEven(value) ? 'yes' : 'no';
+    const userUnswer = readlineSync.question(value);
+    if (userUnswer === expectedAnswer) {
+      console.log('Correct!');
+      i += 1;
+    } else console.log('Wrong answer!')
+    return;
+  }
+  console.log(`Congratulations, ${userName}`);
 };
 
 export default isEvenGame;
