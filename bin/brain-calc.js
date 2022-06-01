435217a4dc288dffa0412671a53ff5241b625a23
Igor Nazim <igor.mainevent@gmail.com>
2022-06-01 12:54:08 +0300

    calc game

---
+++ bin/brain-calc.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import isEvenGame from '../src/brain-calc.js';
+
+calcGame();
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import readlineSync from 'readline-sync';
+import { Random } from 'random-js';
+
+
