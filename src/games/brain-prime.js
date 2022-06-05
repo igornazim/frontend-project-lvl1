107a0cc7ddb402055bed526c22eb1a764005e34c
Igor Nazim <igor.mainevent@gmail.com>
2022-06-05 13:54:24 +0300

    nine step

---
+++ src/games/brain-prime.js
@@ -0,0 +1,39 @@
+import readlineSync from 'readline-sync';
+import { Random } from 'random-js';
+
+const rounds = 3;
+
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const primeGame = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  for (let i = 0; i < rounds; i += 1) {
+    const random = new Random();
+    const value = random.integer(1, 100);
+    console.log(`${value}`);
+    const expectedAnswer = isPrime(value) ? 'yes' : 'no';
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer !== expectedAnswer) {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${expectedAnswer}.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${userName}`);
+};
+
+export default primeGame;
