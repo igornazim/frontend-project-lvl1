3c400201873119be6ff7ad14c6ffb7c0b2feb489
Igor Nazim <igor.mainevent@gmail.com>
2022-07-15 00:53:20 +0300

    Рефакторинг после третьей проверки

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -6,19 +6,15 @@ const makeProgression = (startNumber, step, changeIndex) => {
   const progressionLength = 10;
   let question = '';
   for (let i = 0; i < progressionLength; i += 1) {
-    if (i === changeIndex) {
-      question = `${question} ..`;
-    } else {
-      question = `${question} ${startNumber + (i * step)}`;
-    }
+    question = (i === changeIndex) ? `${question} ..` : `${question} ${startNumber + (i * step)}`;
   }
   return question;
 };
 
 const generateRound = () => {
-  const progressionStart = getRandom();
-  const progressionStep = getRandom(2, 10);
-  const replaceIndex = getRandom(0, 9);
+  const progressionStart = getRandom(1, 100);
+  const progressionStep = getRandom(1, 5);
+  const replaceIndex = getRandom(2, 9);
   const question = makeProgression(progressionStart, progressionStep, replaceIndex);
   const expectedAnswer = String(progressionStart + (progressionStep * replaceIndex));
   return [expectedAnswer, question];
