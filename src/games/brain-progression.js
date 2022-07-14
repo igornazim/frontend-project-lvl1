e95010dce23fd3264719a6dc79e42ee8f0eee322
Igor Nazim <igor.mainevent@gmail.com>
2022-07-15 02:07:12 +0300

    Рефакторинг после третьей проверки

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -3,17 +3,18 @@ import getRandom from '../utils.js';
 
 const gameRule = 'What number is missing in the progression?';
 const makeProgression = (startNumber, step, changeIndex) => {
+  const progressionLength = 10;
   let progression = '';
-  for (let i = 0; i < 9; i += 1) {
+  for (let i = 0; i < progressionLength; i += 1) {
     progression = (i === changeIndex) ? `${progression} ..` : `${progression} ${startNumber + (i * step)}`;
   }
-  return progression;
+  return progression.trim();
 };
 
 const generateRound = () => {
-  const progressionStart = getRandom(1, 100);
-  const progressionStep = getRandom(1, 5);
-  const replaceIndex = getRandom(2, 9);
+  const progressionStart = getRandom();
+  const progressionStep = getRandom(1, 10);
+  const replaceIndex = getRandom(1, 9);
   const question = makeProgression(progressionStart, progressionStep, replaceIndex);
   const expectedAnswer = String(progressionStart + (progressionStep * replaceIndex));
   return [expectedAnswer, question];
