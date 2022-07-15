db126e3da52b47f5a7909ed496222fc3c8ef2e87
Igor Nazim <igor.mainevent@gmail.com>
2022-07-15 14:18:51 +0300

    Наполнение readme

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -2,8 +2,8 @@ import playGame from '../index.js';
 import getRandom from '../utils.js';
 
 const gameRule = 'What number is missing in the progression?';
+const progressionLength = getRandom(5, 10);
 const makeProgression = (startNumber, step, changeIndex) => {
-  const progressionLength = 10;
   let progression = '';
   for (let i = 0; i < progressionLength; i += 1) {
     progression = (i === changeIndex) ? `${progression} ..` : `${progression} ${startNumber + (i * step)}`;
@@ -14,7 +14,7 @@ const makeProgression = (startNumber, step, changeIndex) => {
 const generateRound = () => {
   const progressionStart = getRandom();
   const progressionStep = getRandom(1, 10);
-  const replaceIndex = getRandom(1, 9);
+  const replaceIndex = getRandom(1, progressionLength - 1);
   const question = makeProgression(progressionStart, progressionStep, replaceIndex);
   const expectedAnswer = String(progressionStart + (progressionStep * replaceIndex));
   return [expectedAnswer, question];
